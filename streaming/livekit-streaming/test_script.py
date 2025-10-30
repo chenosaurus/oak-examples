4a103df23e6c5ed6cd7da0c639585322d62960c2
David Chen <chenosaurus@gmail.com>
2025-10-30 16:24:37 -0700

    update test script

---
+++ streaming/livekit-streaming/test_script.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env -S uv run --script
+# /// script
+# dependencies = [
+#   "depthai",
+# ]
+# ///
+import depthai as dai
+from utils.arguments import initialize_argparser
+
+_, args = initialize_argparser()
+
+device = dai.Device(dai.DeviceInfo(args.device)) if args.device else dai.Device()
+
+with dai.Pipeline(device) as pipeline:
+    cam = pipeline.create(dai.node.Camera).build()
+    fps = args.fps_limit or 30
+    raw_stream = cam.requestOutput(
+        (1280, 720), dai.ImgFrame.Type.NV12, fps=fps
+    )
+
+    vid_enc = pipeline.create(dai.node.VideoEncoder)
+    vid_enc.setDefaultProfilePreset(
+        fps, dai.VideoEncoderProperties.Profile.H264_BASELINE
+    )
+    vid_enc.setBitrateKbps(1000)
+    # Shorter GOP for faster viewer startup
+    gop = min(30, fps) if fps else 30
+    vid_enc.setKeyframeFrequency(gop)
+    raw_stream.link(vid_enc.input)
+
+   # Script node will open a TCP socket and forward encoded bytes
+    script = pipeline.create(dai.node.Script)
+    vid_enc.bitstream.link(script.inputs['h264'])
+
+    script.setScript(f"""
+    import socket
+    srv = socket.socket(); srv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    srv.bind(("0.0.0.0", {args.port})); srv.listen(1)
+    conn, _ = srv.accept()
+    while True:
+        pkt = node.io['h264'].get()
+        conn.sendall(pkt.getData())
+    """)
+
+    # Start TCP server to emit encoded frames
+    pipeline.start()
+    print("Running pipeline...")
+
+    while pipeline.isRunning():
+        # Drain encoder output and invoke callback for every encoded frame
+        while True:
+            device.getQueueEvents() 
+    pipeline.stop()
